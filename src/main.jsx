5d777717deff9ba5e6d511b28309c26f06ff2398
TigerXV <stevenmanuel2009@gmail.com>
2022-08-05 15:45:46 -0400

    paquete de depedencias arreglada

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom'
+import { LoginPage } from "./Components/Login/LoginPage";
 import { MedicoApp } from "./Components/MedicoUserView/MedicoApp";
 
 
